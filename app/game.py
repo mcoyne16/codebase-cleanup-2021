be5d4169b16f1895c82a723818bb25b3b3b096e1
mcoyne16 <77979084+mcoyne16@users.noreply.github.com>
2021-03-23 15:57:57 -0300

    Refactored valid options in our game

--- app/game.py
+++ app/game.py
@@ -5,9 +5,11 @@ from random import choice
 # USER SELECTION
 #
 
+Valid_Options = ["rock", "paper", "scissors"]
+
 u = input("Please choose one of 'Rock', 'Paper', or 'Scissors': ").lower()
 print("USER CHOICE:", u)
-if u not in ["rock", "paper", "scissors"]:
+if u not in Valid_Options:
     print("OOPS, TRY AGAIN")
     exit()
 
@@ -15,7 +17,7 @@ if u not in ["rock", "paper", "scissors"]:
 # COMPUTER SELECTION
 #
 
-c = choice(["rock", "paper", "scissors"])
+c = choice(Valid_Options)
 print("COMPUTER CHOICE:", c)
 
 #
