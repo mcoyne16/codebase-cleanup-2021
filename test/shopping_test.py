ac7149c89615eb4c6cdcaeb58dea5e21934e2b76
mcoyne16 <77979084+mcoyne16@users.noreply.github.com>
2021-03-24 13:15:25 -0300

    Test price formatting logic (#2)

--- test/shopping_test.py
+++ test/shopping_test.py
@@ -1,4 +1,11 @@
 
+
+from app.shopping import format_usd
 # TODO: import some code
 
 # TODO: test the code
+
+def test_format_usd():
+    #assert 2 == 2
+    assert format_usd(9.5) == "$9.50"
+#once we have tests...
